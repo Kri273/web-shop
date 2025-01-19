24060c65ec89c21841b7dac6a6dda1185bf93a36
Kri273 <kristelle.tasane@voco.ee>
2025-01-19 16:57:41 +0200

    Create shop product list and product details views and add data to views

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,7 +3,6 @@ const db = require('../utils/db')
 const getProducts = async (req, res) => {
     try {
         const [products] = await db.execute('SELECT * FROM products');
-        console.log(products)
         res.render('index', {
             products: products,
             adminView: false
@@ -11,8 +10,36 @@ const getProducts = async (req, res) => {
     } catch (err) {
         console.error('Error fetching products: ', err);
         res.status(500).send('Error fetching products');
-     }
+    }
 };
 
+const getJustProducts = async (req, res) => {
+    try {
+        const [products] = await db.execute('SELECT * FROM products');
+        res.render('products', {
+            products: products,
+            adminView: false
+        });
+    } catch (err) {
+        console.error('Error fetching products: ', err);
+        res.status(500).send('Error fetching products');
+    }
+};
 
-module.exports = { getProducts };
+const getProductDetails = async (req, res) => {
+    const productId = req.params.productId; // võtame ID URL-ist
+    try {
+        const [product] = await db.execute('SELECT * FROM products WHERE id = ?', [productId]);
+        if (product.length === 0) {
+            return res.status(404).render('404');
+        }
+        res.render('product-detail', {
+            product: product[0]
+        });
+    } catch (err) {
+        console.error('Error fetching product details: ', err);
+        res.status(500).send('Error fetching product details');
+    }
+};
+
+module.exports = { getProducts, getJustProducts, getProductDetails };
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,10 @@
-const express = require('express');
+const express = require("express");
+const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-const shopController = require('../controllers/shop');
-
-router.get('/', (req, res, next) => {
-    res.send('<b>Web shop page</b>');
-});
-router.get('/', shopController.getProducts);
-
+router.get("/", shopController.getProducts);
+router.get("/products", shopController.getJustProducts);
+router.get("/products/:productId", shopController.getProductDetails);
 
 module.exports = router;
